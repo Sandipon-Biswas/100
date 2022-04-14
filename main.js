8655a3db7d0a15fe5487316f645c22693f42f25c
sandipon biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2022-04-14 12:19:22 +0600

    Update main.js

--- main.js
+++ main.js
@@ -13,49 +13,49 @@ var img3 = document.querySelector("#img3");
 var b4 = document.querySelector("#b4");
 var img4 = document.querySelector("#img4");
 
-for (let x = 0; x < 5; x++) {
+
     
     setTimeout(() => {
         img1.classList.remove("black")
         b1.classList.remove("opr")
-    }, 1000);
+    }, 800);
     setTimeout(() => {
         img1.classList.add("black")
         b1.classList.add("opr")
-    }, 10000);
+    }, 5000);
     
     
     
     setTimeout(() => {
         img2.classList.remove("black")
         b2.classList.remove("opr")
-    }, 10000);
+    }, 5000);
     setTimeout(() => {
         img2.classList.add("black")
         b2.classList.add("opr")
-    }, 20000);
+    }, 9000);
     
     
     setTimeout(() => {
         img3.classList.remove("black")
         b3.classList.remove("opr")
-    }, 20000);
+    }, 9000);
     setTimeout(() => {
         img3.classList.add("black")
         b3.classList.add("opr")
-    }, 30000);
+    }, 13000);
     
     
     
     setTimeout(() => {
         img4.classList.remove("black")
         b4.classList.remove("opr")
-    }, 30000);
+    }, 13000);
     setTimeout(() => {
         img4.classList.add("black")
         b4.classList.add("opr")
-    }, 40000);
+    }, 17000);
     
     
-}
+
 
